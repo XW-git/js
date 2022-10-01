7ce34100e41b0022517afd0ac02f0a424af1231b
LuoXW <luoxw2012@gmail.com>
2022-10-01 12:25:31 -0600

    Now AJAX only run whn button is clicked

--- json-ajax/main.js
+++ json-ajax/main.js
@@ -2,9 +2,16 @@
 https://www.youtube.com/watch?v=rJesac0_Ftw
 */
 
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-ourRequest.onload = function() {
-    console.log(ourRequest.responseText);
-};
-ourRequest.send();
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.onload = function() {
+        var ourData=JSON.parse(ourRequest.responseText);
+        console.log(ourData);
+    };
+    ourRequest.send();
+})
+
+
